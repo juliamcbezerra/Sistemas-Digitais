1861039947ad7a446c84c78bb29af07dac315b90
Júlia Maria <jujumcb8@gmail.com>
2025-02-04 09:58:39 -0300

    Create Nível Aplicar - Funções e Pilhas.py

---
+++ Nível Aplicar - Funções e Pilhas.py
@@ -0,0 +1,14 @@
+def soma(a, b):
+    return a + b  # Soma os dois números e retorna o resultado
+
+def main():
+    x = 5
+    y = 3
+    resultado = soma(x, y)  # Chama a função soma
+    print("Resultado:", resultado)  # Exibe o resultado
+
+if __name__ == "__main__":
+    main()
+
+
+
